8aa5f9a2830ca9dcce74e39c0ca896a0916ed7de
VieiraMidas <guilherme.brasil3@etec.sp.gov.br>
2020-06-29 23:32:35 -0300

    Ligação com o Teeeeu

--- app/src/main/java/com/example/film_aplication/MainActivity.java
+++ app/src/main/java/com/example/film_aplication/MainActivity.java
@@ -2,15 +2,29 @@ package com.example.film_aplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    
+    Button btncadastro;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btncadastro = findViewById(R.id.btnCadastro);
+
+        btncadastro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), Activity_cadastrar.class));
+                finish();
+                MainActivity.super.onBackPressed();
+            }
+        });
     }
 }
